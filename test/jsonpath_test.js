e93f105fef0a7478bbc0f5f55f9bdf8fa747d7cd
ferron <ferronrsmith@gmail.com>
2016-06-20 02:15:30 -0400

    added subseek autowrapping functionality

--- test/jsonpath_test.js
+++ test/jsonpath_test.js
@@ -34,6 +34,86 @@ describe('JSOMPath Object Transform', function () {
                     }
                 ]
             });
-    })
+    });
+});
+
+
+describe('Cities', function () {
+
+    var cities_payload = {
+        "cities": [
+            {"city": "Toronto", "population": 2.5},
+            {"city": "New York", "population": 8.1},
+            {"city": "San Diego", "population": 1.6},
+            {"city": "Seattle", "population": 0.65}
+        ]
+    };
+
+
+    it('transformation #1', function () {
+
+        // works
+        var template = {
+            bigCities: ["$.cities[?(@.population > 2)]"]
+        };
+
+        expect(transform(cities_payload, template)).to.deep.equal({
+            "bigCities": [
+                {
+                    "city": "Toronto",
+                    "population": 2.5
+                },
+                {
+                    "city": "New York",
+                    "population": 8.1
+                }
+            ]
+        });
+    });
+
+    it('transformation #2', function () {
+
+        // works
+        var template = {
+            bigCities: ["$.cities", {"name": "city"}]
+        };
+
+        expect(transform(cities_payload, template)).to.deep.equal({
+            "bigCities": [
+                {
+                    "name": "Toronto"
+                },
+                {
+                    "name": "New York"
+                },
+                {
+                    "name": "San Diego"
+                },
+                {
+                    "name": "Seattle"
+                }
+            ]
+        });
+    });
+
+    it('transformation #3', function () {
+
+        // works
+        var template = {
+            bigCities: ["$.cities[?(@.population > 2)]", {name: "city"}]
+        };
+
+        expect(transform(cities_payload, template)).to.deep.equal({
+            "bigCities": [
+                {
+                    "name": "Toronto"
+                },
+                {
+                    "name": "New York"
+                }
+            ]
+        });
+    });
+
 
 });
